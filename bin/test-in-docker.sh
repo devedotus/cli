0861d9209f51211fb9cf6736c2a46fbd9f93431e
Thomas Stachl <thomas@stachl.me>
2018-02-28 15:34:04 -0800

    only create image if it doesn't exist

--- bin/test-in-docker.sh
+++ bin/test-in-docker.sh
@@ -2,5 +2,7 @@
 
 set -ex
 
-docker build -t doctor-command-test -f Dockerfile_for_testing .
+if [[ "$(docker images -q doctor-command-test 2> /dev/null)" == "" ]]; then
+  docker build -t doctor-command-test -f Dockerfile_for_testing .
+fi
 docker run -it --rm doctor-command-test
